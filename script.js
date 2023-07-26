2eb14a4f44b50eb02cc746047c42f99df84686dd
Davide Chirichella <davidechirichella10@gmail.com>
2023-07-26 23:51:57 +0200

    Fine 26/07

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function changeBG(){
     var navbar = document.getElementById('header');
     var name = document.getElementById('name');
+    var remainingNav = document.getElementById('navbar');
     var scrollValue = window.scrollY;
     if (scrollValue<900){
-        navbar.style.backgroundColor = 'white';
+        navbar.style.backgroundColor = '#F8F2D4';
+        remainingNav.style.boxShadow = '0px 0px 0px';
         name.style.color='black';
     }else{
         navbar.style.backgroundColor = 'transparent';
-        name.style.color='white';
+        remainingNav.style.boxShadow = '10px 10px 10px black';
+        name.style.color='#F8F2D4';
     }
 }
 
